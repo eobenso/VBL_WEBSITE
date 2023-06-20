f4d94c9f695b3dd7c7c52e67ccac047542886b93
ebenso <ebenso.students.cs.ubc.ca>
2023-06-19 17:30:04 -0700

    fixed color

--- vbl-website/src/components/MainPage/index.js
+++ vbl-website/src/components/MainPage/index.js
@@ -52,6 +52,7 @@ const WelcomePageContent = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </section>
 
+            <div className='border'>
             <div className='carousel' >
             <h2>Community Bulletin</h2>
 
@@ -108,6 +109,7 @@ const WelcomePageContent = () => {
             </img>
             </div>
             </Slider>
+            </div>
         </div>
 
 
