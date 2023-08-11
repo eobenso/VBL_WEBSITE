1017b014cbc43ee4f009a78d346a849262011f4c
bpark <byungsun1819@gmail.com>
2023-08-10 22:57:40 -0700

    first draft completed

--- vbl-website/src/components/JobOpportunities/index.js
+++ vbl-website/src/components/JobOpportunities/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import './index.css';
-//import image from './images/unnamed.jpg'; 
+import Jobs from './jobs'
+import Job from './job'
+import data from './data.json'
 
 let num_postings = 0;
 
 const Content = () => {
+
     return (
         <div className='Job-Opportunities'>
             <h4 className='sub-section-name'>Community Engagement {'>'} Job Opportunities </h4>
             <br></br>
             <br></br>
-            <div class='Postings-Header'>
+            <div className='Postings-Header'>
               <h1 className='upper heading-name'>Current Listings</h1>
-              <div class='num-postings'> {num_postings} RESULTS</div>
+              <div className='num-postings'> {num_postings} RESULTS</div>
             </div>
             <div className="Job-Board">
-              <div>
-              </div>
+              <Jobs data={data} />
             </div>
         </div>
         )
